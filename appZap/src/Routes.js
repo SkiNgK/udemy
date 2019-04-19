e4f7eeb4f85f5723737515536965bad56690b8a0
Flavio Costa <Skingk.f@gmail.com>
2019-04-19 16:22:25 -0300

    Adicionando tabs para a pagina principal do app com conversas e contatos
    Signed-off-by: Flavio Costa <Skingk.f@gmail.com>

--- appZap/src/Routes.js
+++ appZap/src/Routes.js
@@ -4,7 +4,8 @@ import { Router, Scene } from 'react-native-router-flux';
 import FormLogin from './components/FormLogin';
 import FormCadastro from './components/FormCadastro';
 import TelaInicial from './components/TelaInicial';
-import Principal from './components/Princial';
+import Principal from './components/Principal';
+import AdicionarContatos from './components/AdicionarContatos'
 
 export default props => (
     <Router navigationBarStyle={{ backgroundColor: '#115E54' }} titleStyle={{ color: '#fff'}}>
@@ -12,5 +13,6 @@ export default props => (
         <Scene key='formLogin' component={FormLogin} hideNavBar={true}/>
         <Scene key='formCadastro' component={FormCadastro} title="Cadastro" hideNavBar={false}/>
         <Scene key='principal' component={Principal} title="Tela principal" hideNavBar={true}/>
+        <Scene key='adicionarContatos' component={AdicionarContatos} title="Adicionar Contatos" hideNavBar={false}/>
     </Router>
 );
---
+++ appZap/src/components/Contatos.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { View, Text, ListView } from 'react-native';
+import { connect } from 'react-redux';
+import _ from 'lodash';
+
+import { contatosUsuarioFetch } from '../actions/AppActions';
+
+class Contatos extends Component {
+    componentWillMount() {
+        this.props.contatosUsuarioFetch();
+        //recebe os dados vazios de contatos vazios 
+        //e usando esses dados para inicializar a variável fonteDeDados
+        this.criaFonteDeDados(this.props.contatos);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        //passando os dados dos contatos para a variavel fonteDeDados
+        this.criaFonteDeDados(nextProps.contatos);
+    }
+
+    criaFonteDeDados( contatos ){
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+        this.fonteDeDados = ds.cloneWithRows(contatos)
+    }
+
+    render() {
+        return (
+            <ListView 
+            enableEmptySections
+            dataSource={this.fonteDeDados}
+            renderRow={data => 
+                <View style={{flex: 1, padding: 20, borderBottomWidth: 1, borderColor: '#CCC'}}>
+                    <Text style={{fontSize: 25}}>
+                        {data.nome}
+                    </Text>
+                    <Text style={{fontSize: 18}}>
+                        {data.email}
+                    </Text>
+                </View>
+                }
+            />
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    const contatos = _.map(state.ListaContatosReducer, (val, uid) => {
+        return { ...val, uid}
+    })
+    return {
+        contatos
+    }
+}
+
+export default connect(mapStateToProps, { contatosUsuarioFetch })(Contatos);
