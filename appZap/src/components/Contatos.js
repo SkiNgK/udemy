145dea07b98b2b7463cabdfb3eed85c6be783a2c
Flavio Costa <Skingk.f@gmail.com>
2019-04-22 17:35:42 -0300

    Reafatorando contatos para envio de props via actions para conversas
    Signed-off-by: Flavio Costa <Skingk.f@gmail.com>

--- appZap/src/components/Contatos.js
+++ appZap/src/components/Contatos.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text, ListView } from 'react-native';
+import { View, Text, ListView, TouchableHighlight } from 'react-native';
 import { connect } from 'react-redux';
 import _ from 'lodash';
+import { Actions} from 'react-native-router-flux'
 
 import { contatosUsuarioFetch } from '../actions/AppActions';
 
@@ -18,10 +19,30 @@ class Contatos extends Component {
         this.criaFonteDeDados(nextProps.contatos);
     }
 
+    //logica para instanciar o datasource
     criaFonteDeDados( contatos ){
         const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 
         this.fonteDeDados = ds.cloneWithRows(contatos)
+    }
+
+    renderRow(contato){
+        return(
+            //o redux permite a passagem de parametros por actions do routerflux
+            <TouchableHighlight onPress={() => Actions.conversa({ 
+                title: contato.nome,
+                contatoNome: contato.nome, 
+                contatoEmail: contato.email})}>
+                <View style={{flex: 1, padding: 20, borderBottomWidth: 1, borderColor: '#CCC'}}>
+                    <Text style={{fontSize: 25}}>
+                        {contato.nome}
+                    </Text>
+                    <Text style={{fontSize: 18}}>
+                        {contato.email}
+                    </Text>
+                </View>
+            </TouchableHighlight>
+        )
     }
 
     render() {
@@ -29,16 +50,11 @@ class Contatos extends Component {
             <ListView 
             enableEmptySections
             dataSource={this.fonteDeDados}
-            renderRow={data => 
-                <View style={{flex: 1, padding: 20, borderBottomWidth: 1, borderColor: '#CCC'}}>
-                    <Text style={{fontSize: 25}}>
-                        {data.nome}
-                    </Text>
-                    <Text style={{fontSize: 18}}>
-                        {data.email}
-                    </Text>
-                </View>
-                }
+
+            //a função recebe por parametro em cada uma das interações 
+            //do dataSource o registro a ser renderizado
+            renderRow={data => this.renderRow(data)}
+            //renderRow={this.renderRow} é a mesma coisa da função de cima
             />
         )
     }
--- appZap/src/reducers/ListaContatosReducer.js
+++ appZap/src/reducers/ListaContatosReducer.js
@@ -5,7 +5,7 @@ const INITIAL_STATE = {
 }
 
 export default (state = INITIAL_STATE, action) => {
-    console.log('teste', action.payload)
+    // console.log('teste', action.payload)
     switch (action.type) {
         case LISTA_CONTATO_USUARIO:
             return action.payload
